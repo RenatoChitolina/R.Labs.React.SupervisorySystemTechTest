c8701b7e81e443c50296b36c147dcacd8116c17e
Renato Chitolina <renato.chitolina@immutable.com>
2022-03-17 12:17:59 -0300

    Replacing the interaction with the backend through a random list of sensors, to use a request to retrieve the list of real available sensors to work with. Adding backend interaction through the service. Removing the business rule to only get the information already processed by the backend. Adding some more information on the UI and doing some layout adjustments.

--- src/App.js
+++ src/App.js
@@ -1,67 +1,37 @@
 import { useEffect, useState } from 'react';
-
-const data = [
-  {
-    id: '1',
-    name: 'Pilsner',
-    minimumTemperature: 4,
-    maximumTemperature: 6,
-  },
-  {
-    id: '2',
-    name: 'IPA',
-    minimumTemperature: 5,
-    maximumTemperature: 6,
-  },
-  {
-    id: '3',
-    name: 'Lager',
-    minimumTemperature: 4,
-    maximumTemperature: 7,
-  },
-  {
-    id: '4',
-    name: 'Stout',
-    minimumTemperature: 6,
-    maximumTemperature: 8,
-  },
-  {
-    id: '5',
-    name: 'Wheat beer',
-    minimumTemperature: 3,
-    maximumTemperature: 5,
-  },
-  {
-    id: '6',
-    name: 'Pale Ale',
-    minimumTemperature: 4,
-    maximumTemperature: 6,
-  },
-];
+import { sensorProvider } from './services/sensorProvider';
 
 function App() {
-  const [items, setItems] = useState({});
+  const [availableSensors, setAvailableSensors] = useState([]);
+  const [sensors, setSensors] = useState({});
 
   useEffect(() => {
     const request = () =>
-      data.forEach((product) => {
-        fetch(`http://localhost:8081/temperature/${product.id}`)
-          .then((response) => response.json())
-          .then((response) =>
-            setItems((prevItems) => ({
-              ...prevItems,
-              [product.id]: {
-                ...product,
-                ...response,
-              },
-            }))
-          );
-      });
-
+      sensorProvider
+        .getAvailableSensors()
+        .then((response) => setAvailableSensors(response));
+    
     setInterval(request, 5000);
-
+    
     request();
   }, []);
+
+  useEffect(() => {
+    availableSensors.forEach(sensorId => {
+      sensorProvider
+        .getSensorTemperature(sensorId)
+        .then((response) => {
+          setSensors(prevSensors => ({
+            ...prevSensors,
+            [sensorId]: {
+              ...sensorId,
+              ...response,
+            }
+          }))
+        }
+        );
+      });
+  }, [availableSensors]);
 
   return (
     <div className="App">
@@ -69,26 +39,17 @@ function App() {
       <table>
         <thead>
           <tr>
-            <th align="left">Product</th>
-            <th align="left">Temperature</th>
-            <th align="left">Status</th>
+            <th align="left">Product (Min - Max)</th>
+            <th align="right">Temperature</th>
+            <th align="center">Status</th>
           </tr>
         </thead>
         <tbody>
-          {Object.keys(items).map((itemKey) => (
-            <tr key={items[itemKey].id}>
-              <td width={150}>{items[itemKey].name}</td>
-              <td width={150}>{items[itemKey].temperature}</td>
-              <td width={150}>
-                {items[itemKey].temperature <
-                  items[itemKey].minimumTemperature && <span>too low</span>}
-                {items[itemKey].temperature >
-                  items[itemKey].maximumTemperature && <span>too high</span>}
-                {items[itemKey].temperature <=
-                  items[itemKey].maximumTemperature &&
-                  items[itemKey].temperature >=
-                    items[itemKey].minimumTemperature && <span>all good</span>}
-              </td>
+          {Object.keys(sensors).map(sensorKey => (
+            <tr key={sensors[sensorKey].id}>
+              <td align="left" width={200}>{sensors[sensorKey].name} ({sensors[sensorKey].minimumTemperature} - {sensors[sensorKey].maximumTemperature})</td>
+              <td align="right" width={200}>{sensors[sensorKey].temperature}</td>
+              <td align="center" width={200}>{sensors[sensorKey].status}</td>
             </tr>
           ))}
         </tbody>
